bc1942b6dc3bda3c7eb2251dcebb07325e6b89f5
Aliakbar Imanov <aliakbar.imanov@gmail.com>
2023-10-26 14:42:55 +0400

    add registration form error messages

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,72 +1,94 @@
 // import useState
 import { useState } from "react";
 
-import {useForm} from "react-hook-form";
-import {object, string} from "yup";
+// import useForm
+import { useForm } from "react-hook-form";
+
+// import object, string
+import { object, string } from "yup";
+
+// import yupResolver
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Registartion = () => {
+  const registerSchema = object({
+    name: string()
+      .required("Adinizi daxil etmediniz.")
+      .trim("Adinizi daxil etmediniz."),
+    surname: string()
+      .required("Soyadinizi daxil etmediniz.")
+      .trim("Soyadinizi daxil etmediniz."),
+    email: string()
+      .required("Emailinizi daxil etmediniz.")
+      .trim("Emailinizi daxil etmediniz.")
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Email-i duzgun formatda daxil etmediniz."
+      ),
+    password: string()
+      .required("Parolu daxil etmediniz.")
+      .trim("Parolu daxil etmediniz.")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/,
+        "Parol min 8, en az bir reqem, en az bir boyuk, bir balaca herf olamlidir."
+      ),
+  });
 
-    const registerSchema = object({
-        name: string(),
-        surname: string(),
-        email: string(),
-        password: string(),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
 
-    const {register, handleSubmit, formState: {errors}} = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    // const handleChange = (e) => {
-
-    //     let inpName = e.target.name;
-    //     let value = e.target.value;
-
-    //     setNewUser({...newUser, [inpName]:value});
-    // }
-
-    // const onSubmit = (e) => {
-    //     e.preventDefault();
-    //     if(newUser.name===""){
-    //         alert("Bos");
-    //     }
-    // }
-
-    console.log(newUser);
-    
-    return (
-        <section className="login">
-            <div className="container">
-                <div className="row">
-                    <h2>Registration Page</h2>
-                    <div className="login-box">
-                        <form onSubmit={onSubmit}>
-                            <div className="user-box">
-                                <input type="text" name="name" onChange={handleChange} />
-                                <label>Name</label>
-                            </div>
-                            <div className="user-box">
-                                <input type="text" name="surname" onChange={handleChange} />
-                                <label>Surname</label>
-                            </div>
-                            <div className="user-box">
-                                <input type="email" name="email" onChange={handleChange} />
-                                <label>Email</label>
-                            </div>
-                            <div className="user-box">
-                                <input type="password" name="password" onChange={handleChange} />
-                                <label>Password</label>
-                            </div>
-                            <div>
-                                <button>
-                                    Register
-                                    <span></span>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="login">
+      <div className="container">
+        <div className="row">
+          <h2>Registration Page</h2>
+          <div className="login-box">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className={errors.name ? "user-box error" : "user-box"}>
+                <input type="text" name="name" {...register("name")} />
+                <label>Name</label>
+                {errors.name && <span className="errorMsg">{errors.name.message}</span>}
+              </div>
+              <div className={errors.surname ? "user-box error" : "user-box"}>
+                <input type="text" name="surname" {...register("surname")} />
+                <label>Surname</label>
+                {errors.surname && <span className="errorMsg">{errors.surname.message}</span>}
+              </div>
+              <div className={errors.email ? "user-box error" : "user-box"}>
+                <input type="email" name="email" {...register("email")} />
+                <label>Email</label>
+                {errors.email && <span className="errorMsg">{errors.email.message}</span>}
+              </div>
+              <div className={errors.password ? "user-box error" : "user-box"}>
+                <input
+                  type="password"
+                  name="password"
+                  {...register("password")}
+                />
+                <label>Password</label>
+                {errors.password && <span className="errorMsg">{errors.password.message}</span>}
+              </div>
+              <div>
+                <button>
+                  Register
+                  <span></span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Registartion;
