2dfcd8674ee4da9373179ffd84e163f7bcb3e5b4
aliakbarimanov <aliakbar.imanov@gmail.com>
2023-11-01 00:17:19 +0400

    create user profile image change

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,26 @@
 import { useContext } from "react";
 import { Context } from "../utils/MainContext";
+import axios from "axios";
 
 const Profile = () => {
 
-    const { user } = useContext(Context);
+    const { user, setUser } = useContext(Context);
+
+    const onChange = async (e) => {
+
+        const token = JSON.parse(localStorage.getItem("token"));
+        const profileImage = e.target.files[0];
+
+        const body = new FormData();
+        body.append("token", token);
+        body.append("profileImage", profileImage);
+
+        await axios.put(process.env.REACT_APP_CHANGE_USER_PROFILE_IMAGE, body)
+            .then(res => {
+                setUser(res.data);
+            })
+            .catch(err => console.log(err));
+    }
 
     return (
         <section className="profilePage">
@@ -12,12 +29,19 @@ const Profile = () => {
                     <h1 className="profileTitle">Welcome {user.name} {user.surname}</h1>
                     <div className="profileBox">
                         <div className="profileImg">
-                            <img src="https://freepngimg.com/thumb/strawberry/1-strawberry-png-images.png" alt="profile-image" />
+                            {
+                                user.profileImage ? (
+                                    <img src={`http://localhost:8000/${user.profileImage}`} alt="profile-image" />
+                                ) : (
+                                    <img src="https://freepngimg.com/thumb/strawberry/1-strawberry-png-images.png" alt="profile-image" />
+                                )
+                            }
                         </div>
                         <div className="profileInfo">
                             <h2 className="userInfo">{user.name}</h2>
                             <h2 className="userInfo">{user.surname}</h2>
                             <h2 className="userInfo">{user.email}</h2>
+                            <input type="file" onChange={onChange} />
                         </div>
                     </div>
                 </div>
